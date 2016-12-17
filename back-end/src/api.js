c71f41e0854fc5449782f902e713de17a3f604bc
Ned Burns <net7runner@gmail.com>
2016-12-17 16:02:43 -0500

    Hook up authentication flow to the DB

--- back-end/src/api.js
+++ back-end/src/api.js
@@ -1,8 +1,9 @@
-const dao = require('./dao.js');
 const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+
+const dao = require('./dao.js');
 
 // /api routes
 const router = express.Router();
@@ -12,8 +13,48 @@ function sendStub(res, filepath) {
   res.send(fs.readFileSync(path.join(__dirname, filepath), 'utf8'));
 }
 
+router.get('/*', function(req, res, next) {
+  // TODO: Check to make sure account ID is still valid
+  // and that account has permissions to access this path
+  if (req.session.accountId == null) {
+    res.status(401).send('401 Unauthorized');
+  } else {
+    next();
+  }
+});
+
 router.get('/dashboard', function(req, res) {
-  sendStub(res, '../api-stubs/dashboard.json');
+  let json =
+      JSON.parse(
+          fs.readFileSync(
+              path.join(__dirname, '../api-stubs/dashboard.json'), 'utf8'));
+
+  let accountId = req.session.accountId;
+
+  dao.builder('ownership')
+      .select('character.id', 'character.name', 'accessToken.needsUpdate')
+      .join('character', 'character.id', '=', 'ownership.character')
+      .join('accessToken', 'accessToken.character', '=', 'ownership.character')
+      .where('ownership.account', accountId)
+  .then(function(rows) {
+    for (let i = 0; i < rows.length; i++) {
+      let row = rows[i];
+      json.characters.unshift({
+        id: row.id,
+        name: row.name,
+        hasApiKey: !row.needsUpdate,
+        skillInTraining: null,
+        queue: null,
+      })
+    }
+    res.type('json');
+    res.send(JSON.stringify(json));
+  })
+  .catch(function(err) {
+    // TODO
+    console.log('ERROR:', err);
+    res.send(err);
+  });
 });
 
 // GET -> returns JSON representing entire corp roster.
