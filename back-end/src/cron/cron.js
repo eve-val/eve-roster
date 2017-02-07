6f0fb8bddb5fec499613edaa9968d6876c0450ec
Michael Ludwig <lhkbob@gmail.com>
2017-02-06 19:21:13 -0600

    Remove old ESI syncKillboard
    Moves syncZKillboard contents into syncKillboard, fixes cron reference,
    removes debug log statement that was missed, and sets user-agent based
    on config file.

--- back-end/src/cron/cron.js
+++ back-end/src/cron/cron.js
@@ -20,7 +20,7 @@ const TASKS = [
   },
   {
     name: 'syncKillboard',
-    executor: require('./task/syncZKillboard'),
+    executor: require('./task/syncKillboard'),
     timeout: moment.duration(30, 'minutes').asMilliseconds(),
     interval: moment.duration(1, 'day').asMilliseconds(),
     schedule: '0 2 * * *',  // Once a day at 2AM
