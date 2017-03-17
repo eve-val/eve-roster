ac0a5ac0a4800c1726d2f81ee1378e12a420b2d1
7sempra <net7runner@gmail.com>
2017-03-16 21:18:07 -0400

    Add Jest testing framework (#167)

--- src/util/time.js
+++ src/util/time.js
@@ -36,36 +36,3 @@ module.exports = {
     return out.join(' ');
   },
 };
-
-if (require.main === module) {
-  const path = require('path');
-  const runTest = (testName, start, end, maxUnits, expected) => {
-    const actual = module.exports.shortDurationString(start, end, maxUnits);
-    if (actual !== expected) {
-      console.error(`${testName} expected: ${expected} got: ${actual}`);
-    }
-  };
-  console.log(`Running tests for ${path.basename(__filename)}...`);
-  runTest('testDays',
-    '2017-03-04T14:47:49Z', '2017-03-07T20:51:02Z', 1, '3d');
-  runTest('testDaysHours',
-    '2017-03-04T14:47:49Z', '2017-03-07T20:51:02Z', 2, '3d 6h');
-  runTest('testDaysHoursMinutes',
-    '2017-03-04T14:47:49Z', '2017-03-07T20:51:02Z', 3, '3d 6h 3m');
-  runTest('testOnlyMinutes',
-    '2017-03-04T14:47:49Z', '2017-03-04T14:51:02Z', 1, '3m');
-  runTest('testOnlyMinutesHours',
-    '2017-03-04T14:47:49Z', '2017-03-04T20:51:02Z', 3, '6h 3m');
-  // Treat reversed start and end as a duration
-  runTest('testStartEndReversed',
-    '2017-03-07T20:51:02Z', '2017-03-04T14:47:49Z', 3, '3d 6h 3m');
-  // undefined gets interpreted as Date.now()
-  runTest('testEndUndefined',
-    '2017-03-07T20:51:02Z', undefined, 3,
-    module.exports.shortDurationString('2017-03-07T20:51:02Z', Date.now(), 3));
-  // empty strings give back empty strings - they turn into NaN durations
-  runTest('testStartEmptyString',
-    '', '2017-03-04T14:47:49Z', 3, '');
-  runTest('testEndEmptyString',
-    '2017-03-04T14:47:49Z', '', 3, '');
-}
