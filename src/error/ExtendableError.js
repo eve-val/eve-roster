80d6a74e425ac75320b63c43479a528525727c4c
Ned Burns <net7runner@gmail.com>
2017-04-09 15:13:24 -0400

    Use error name as default message for all custom errors
    If `message` is null, Bluebird won't recognize our custom errors as
    errors and will throw a warning.
    
    Also removes some unneeded code from ExtendableError.

--- src/error/ExtendableError.js
+++ src/error/ExtendableError.js
@@ -2,11 +2,6 @@ module.exports = class ExtendableError extends Error {
   constructor(message) {
     super(message);
     this.name = this.constructor.name;
-    this.message = message; 
-    if (typeof Error.captureStackTrace === 'function') {
-      Error.captureStackTrace(this, this.constructor);
-    } else { 
-      this.stack = (new Error(message)).stack; 
-    }
+    this.message = message || this.name;
   }
 };
