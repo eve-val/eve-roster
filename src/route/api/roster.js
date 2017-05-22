d24956dc1eeb324657bea4ef6b3e5ae65f8445fd
Michael Ludwig <lhkbob@gmail.com>
2017-05-22 15:38:32 -0500

    Add per-character/account warnings to roster api
    Each account and character in main or alts array can have a warning
    message and corresponding warning level.
    
    The API call currently implements a number of warnings, errors, and
    info messages:
    
    1. Main is not in SAFE -> warning
    2. Main is not in any SOUND affiliated corp -> error
    3. Character is not claimed by an account -> warning
    4. Character does not have roles in their corp (assuming it's a SOUND
    corp) -> error.
    5. Account is a trial member -> info

--- src/route/api/roster.js
+++ src/route/api/roster.js
@@ -7,19 +7,37 @@ const eve = require('../../eve');
 const logger = require('../../util/logger')(__filename);
 const protectedEndpoint = require('../../route-helper/protectedEndpoint');
 
+const CHAR_NO_MSG = 0;
+const CHAR_INFO_MSG = 1;
+const CHAR_WARNING_MSG = 2;
+const CHAR_ERROR_MSG = 3;
+
 module.exports = protectedEndpoint('json', (req, res, account, privs) => {
   privs.requireRead('roster');
 
-  return dao.getRosterCharacterCorps()
+  let memberCorps;
+  let corpNames;
+  return dao.config.getMemberCorporations()
+  .then(corpRows => {
+    memberCorps = { mainCorpIds: [], altCorpIds: [] };
+    for (let c of corpRows) {
+      if (c.membership == 'full') {
+        memberCorps.mainCorpIds.push(c.corporationId);
+      } else if (c.membership == 'affiliated') {
+        memberCorps.altCorpIds.push(c.corporationId);
+      }
+    }
+
+    return dao.getRosterCharacterCorps();
+  })
   .then(corpIds => {
     return eve.esi.corporations.names(corpIds);
   })
-  .then(corpNames => {
-    let corpNameMap = new Map();
-    for (let cn of corpNames) {
-      corpNameMap.set(cn.id, cn.name);
+  .then(response => {
+    corpNames = new Map();
+    for (let cn of response) {
+      corpNames.set(cn.id, cn.name);
     }
-    return corpNameMap;
   })
   .catch(e => {
     if (error.isAnyEsiError(e)) {
@@ -27,30 +45,33 @@ module.exports = protectedEndpoint('json', (req, res, account, privs) => {
       // FIXME attach warning to response object once warnings are supported
       // in the roster client view
       logger.error('ESI error fetching corporation names', e);
+      corpNames = null;
       return null;
     } else {
       // Re-throw since it's something more serious
       throw e;
     }
   })
-  .then(corpNames => {
+  .then(() => {
     return Promise.all([
       dao.getCharactersOwnedByMembers(),
       dao.getUnownedCorpCharacters(),
-      Promise.resolve(corpNames) // Pass through
     ]);
   })
-  .then(function([ownedChars, unownedChars, corpNames]) {
+  .then(([ownedChars, unownedChars]) => {
     let accountList = [];
 
-    pushOwnedChars(ownedChars, accountList, privs, corpNames);
+    pushOwnedChars(ownedChars, accountList, privs, memberCorps, corpNames);
 
     for (let unownedChar of unownedChars) {
       accountList.push(
-          getAccountOutput(
-              unownedChar, null, false /* not owned */, privs, corpNames));
+          getAccountOutput(unownedChar, null, false /* not owned */, privs,
+              memberCorps, corpNames));
     }
 
+    return Promise.all(accountList);
+  })
+  .then((accountList) => {
     return {
       columns: getProvidedColumns(privs),
       rows: accountList
@@ -59,7 +80,19 @@ module.exports = protectedEndpoint('json', (req, res, account, privs) => {
 
 });
 
-function pushOwnedChars(ownedRows, outList, privs, corpNames) {
+function addMessage(data, message, level) {
+  if (data.warning) {
+    // Append message and possibly increase level
+    data.warning = data.warning + '\n' + message;
+    data.warningLevel = Math.max(data.warningLevel, level);
+  } else {
+    // No previous message so set as-is
+    data.warning = message;
+    data.warningLevel = level;
+  }
+}
+
+function pushOwnedChars(ownedRows, outList, privs, memberCorps, corpNames) {
   let accountGroups = new Map();
 
   for (let row of ownedRows) {
@@ -83,23 +116,39 @@ function pushOwnedChars(ownedRows, outList, privs, corpNames) {
   }
 
   for (let group of accountGroups.values()) {
-    // TODO: Should we add a warning flag here if !(main.corporation in corps)?
     outList.push(
         getAccountOutput(
-            group.main, group.alts, true /* is owned */, privs, corpNames));
+            group.main, group.alts, true /* is owned */, privs, memberCorps,
+            corpNames));
   }
 }
 
-function getAccountOutput(mainRow, altRows, isOwned, privs, corpNames) {
+function getAccountOutput(mainRow, altRows, isOwned, privs, memberCorps,
+    corpNames) {
   let obj = {
-    main: getCharOutput(mainRow, privs, corpNames),
+    main: getCharOutput(mainRow, privs, memberCorps, corpNames),
     alts: null,
-    isOwned: isOwned,
   };
 
+  if (!isOwned) {
+    addMessage(obj, 'Character is not claimed.', CHAR_WARNING_MSG);
+  }
+
+  let mainInFullCorp = memberCorps.mainCorpIds.includes(mainRow.corporationId);
+  let mainInAffilCorp = memberCorps.altCorpIds.includes(mainRow.corporationId);
+  if (!mainInFullCorp && !mainInAffilCorp) {
+    addMessage(mainRow, 'Main character is not in any affiliated corporation.',
+        CHAR_ERROR_MSG);
+  } else if (!mainInFullCorp) {
+    addMessage(mainRow, 'Main character is not in primary corporation.',
+        CHAR_WARNING_MSG);
+  }
+
   if (altRows != null && privs.canRead('memberAlts')) {
-    // TODO: Filter out external alts if missing memberExternalAlts perm
-    obj.alts = altRows.map(char => getCharOutput(char, privs, corpNames));
+    // pushOwnedChars already filters out opsec alts, so no additional filtering
+    // is necessary.
+    obj.alts = altRows.map(char =>
+        getCharOutput(char, privs, memberCorps, corpNames));
   } else {
     obj.alts = [];
   }
@@ -112,10 +161,35 @@ function getAccountOutput(mainRow, altRows, isOwned, privs, corpNames) {
     obj.homeCitadel = mainRow.homeCitadel || null;
   }
 
-  return obj;
+  // Do not bother checking for memberGroups privilege, since warnings are
+  // accessible to all members, and the trial member status is almost always
+  // determined by a title (which is public info in Eve). But checking via
+  // access groups is more robust here since the roster sync code has already
+  // resolved all custom titles to groups (no need to duplicate).
+  // - This does mean that unclaimed accounts cannot be marked as trial members.
+  if (mainRow.accountId) {
+    return dao.getAccountGroups(mainRow.accountId)
+    .then(groups => {
+      if (groups.includes('provisional_member')) {
+        addMessage(obj, 'Trial member.', CHAR_INFO_MSG);
+      }
+      return obj;
+    });
+  }
+
+  // No further asynchronous data is needed so just return obj
+  return Promise.resolve(obj);
 }
 
-function getCharOutput(row, privs, corpNames) {
+function parseTitles(titleString) {
+  if (titleString) {
+    return JSON.parse(titleString);
+  } else {
+    return [];
+  }
+}
+
+function getCharOutput(row, privs, memberCorps, corpNames) {
   let obj = {
     id: row.id,
     name: row.name,
@@ -123,6 +197,13 @@ function getCharOutput(row, privs, corpNames) {
     corporationName: corpNames ?
         corpNames.get(row.corporationId) : 'Name unavailable'
   };
+
+  let titles = parseTitles(row.titles);
+  let inMemberCorp = memberCorps.mainCorpIds.includes(row.corporationId)
+      || memberCorps.altCorpIds.includes(row.corporationId);
+  if (inMemberCorp && titles.length == 0) {
+    addMessage(obj, 'Character does not have roles.', CHAR_ERROR_MSG);
+  }
 
   if (privs.canRead('characterActivityStats')) {
     let lastSeen;
@@ -149,8 +230,14 @@ function getCharOutput(row, privs, corpNames) {
 }
 
 function getProvidedColumns(privs) {
-  let providedColumns =
-      ['id', 'name', 'corporationId', 'corporationName', 'isOwned'];
+  let providedColumns = [
+    'id',
+    'name',
+    'corporationId',
+    'corporationName',
+    'warning',
+    'warningLevel'
+  ];
 
   if (privs.canRead('memberAlts')) {
     providedColumns.push('alts');
