d19109b79a030261623c550081700b95cec1c068
Michael Ludwig <lhkbob@gmail.com>
2017-06-21 23:03:23 -0700

    Fix roster alert level enum values.

--- src/shared/rosterAlertLevels.js
+++ src/shared/rosterAlertLevels.js
@@ -4,7 +4,7 @@
 export const LEVEL_INFO = 1;
 
 /** @type {number} */
-export const LEVEL_WARNING = 1;
+export const LEVEL_WARNING = 2;
 
 /** @type {number} */
-export const LEVEL_ERROR = 1;
+export const LEVEL_ERROR = 3;
