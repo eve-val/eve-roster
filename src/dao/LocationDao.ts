a61df2bcf4f97c6112370652e94d3d41473fa309
Amber Yust <amber.yust@gmail.com>
2017-07-21 01:17:10 +0000

    Only insert locations when they have changed

--- src/dao/LocationDao.ts
+++ src/dao/LocationDao.ts
@@ -11,7 +11,25 @@ export default class LocationDao {
   }
 
   put(db: Tnex, locationData: Location) {
-    return db.insert(location, locationData);
+    return db
+      .select(location)
+      .where('location_character', '=', val(locationData.location_character))
+      .orderBy('location_timestamp', 'desc')
+      .columns('location_shipName',
+               'location_shipTypeId',
+               'location_shipItemId',
+               'location_solarSystemId',
+               'location_character',
+               'location_timestamp',)
+      .fetchFirst()
+      .then((row: Location | null) => {
+        // Need to compare the actual fields we care about, timestamp changes
+        if (!row || locationsDiffer(row, locationData)) {
+          return db.insert(location, locationData);
+        } else {
+          return 0; // no delta, don't insert
+        }
+      });
   }
 
   deleteOldLocations(db: Tnex, cutoff: number) {
@@ -20,4 +38,12 @@ export default class LocationDao {
         .where('location_timestamp', '<', val(cutoff))
         .run();
   }
+}
+
+function locationsDiffer(a: Location, b: Location) {
+  if (a.location_shipName != b.location_shipName) { return true; }
+  if (a.location_shipTypeId != b.location_shipTypeId) { return true; }
+  if (a.location_shipItemId != b.location_shipItemId) { return true; }
+  if (a.location_solarSystemId != b.location_solarSystemId) { return true; }
+  return false;
 }
