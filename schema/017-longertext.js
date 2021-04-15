57403c6ab66301f694f9502c6c885f25a2e00bf8
Liz Fong-Jones <lizf@honeycomb.io>
2021-04-15 00:45:35 -0400

    make it longer (#467)

---
+++ schema/017-longertext.js
@@ -0,0 +1,14 @@
+/**
+ * Makes the value field for text long enough.
+ */
+exports.up = async function(trx) {
+  await trx.schema.alterTable('characterNotification', (table) => {
+    table.string('text', 2500).notNullable().alter();
+  });
+};
+
+exports.down = async function(trx) {
+  await trx.schema.alterTable('characterNotification', (table) => {
+    table.string('text', 1000).notNullable().alter();
+  });
+};
