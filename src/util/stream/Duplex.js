b36e5de33f0b1922a09723f7fff8dadc1904c7c2
Liz Fong-Jones <lizf@honeycomb.io>
2021-05-31 14:11:16 -0700

    restore deleted js files

---
+++ src/util/stream/Duplex.js
@@ -0,0 +1,1 @@
+export { Duplex } from "stream";
---
+++ src/util/stream/pipeline.js
@@ -0,0 +1,5 @@
+import * as stream from "stream";
+import * as util from "util";
+
+export const pipeline = stream.pipeline;
+export const pipelinePr = util.promisify(stream.pipeline);
