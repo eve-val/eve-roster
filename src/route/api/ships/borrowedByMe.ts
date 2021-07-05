0900e7501d1baf3ed5cf891366b4a3da0181556d
Liz Fong-Jones <lizf@honeycomb.io>
2021-07-05 15:07:16 -0700

    prettier/engine fixup

--- src/route/api/ships/borrowedByMe.ts
+++ src/route/api/ships/borrowedByMe.ts
@@ -2,12 +2,12 @@ import { dao } from "../../../db/dao";
 import { BorrowedShipOutputRow } from "../../../db/dao/CharacterShipDao";
 import { jsonEndpoint } from "../../../infra/express/protectedEndpoint";
 
-export default jsonEndpoint((req, res, db, account, privs): Promise<
-  BorrowedShipOutputRow[]
-> => {
-  privs.requireRead("characterShips", true);
-  return dao.characterShip.getBorrowedShips(db, {
-    accountId: account.id,
-    includeOpsecChars: privs.canRead("memberOpsecAlts", true),
-  });
-});
+export default jsonEndpoint(
+  (req, res, db, account, privs): Promise<BorrowedShipOutputRow[]> => {
+    privs.requireRead("characterShips", true);
+    return dao.characterShip.getBorrowedShips(db, {
+      accountId: account.id,
+      includeOpsecChars: privs.canRead("memberOpsecAlts", true),
+    });
+  }
+);
