3b47ce3c84e23a4a74ac6b776b28b77b85cc9dc0
Ned Burns <net7runner@gmail.com>
2022-09-25 00:45:44 -0400

    Use envalid to parse env variables
    Just setting up preliminary parsing. Will replace all usages of
    process.env with new env container in later CLs.

---
+++ src/infra/init/Env.ts
@@ -0,0 +1,59 @@
+import { bool, cleanEnv, makeValidator, str } from "envalid";
+
+export function initEnv() {
+  return cleanEnv(process.env, {
+    // Must be set manually in all environments
+    COOKIE_SECRET: str(),
+    SSO_CLIENT_ID: str(),
+    SSO_SECRET_KEY: str(),
+    USER_AGENT: str(),
+
+    // Either DATABASE_URL or the other four must be set
+    DATABASE_URL: str({ default: undefined }),
+    DATABASE_HOST: str({ default: undefined }),
+    DATABASE_USER: str({ default: undefined }),
+    DATABASE_NAME: str({ default: undefined }),
+    DATABASE_PASS: str({ default: undefined }),
+
+    // Stuff that's primarily intended to be set in production
+    HOSTNAME: str({ devDefault: "localhost" }),
+    PORT: int({ devDefault: 8081 }),
+
+    DOKKU_NGINX_PORT: int({ devDefault: undefined }),
+    DOKKU_PROXY_PORT: int({ devDefault: undefined }),
+    DOKKU_NGINX_SSL_PORT: int({ devDefault: undefined }),
+    DOKKU_PROXY_SSL_PORT: int({ devDefault: undefined }),
+
+    HONEYCOMB_API_KEY: str({ devDefault: '' }),
+    HONEYCOMB_DATASET: str({ devDefault: '' }),
+
+    // Debugging flags
+    DEBUG_GROUPS: jsonDebugGroups({ default: undefined }),
+    DEBUG_DISABLE_CRON: bool({ default: false }),
+  });
+};
+
+export type Env = ReturnType<typeof initEnv>;
+
+const int = makeValidator(input => {
+  const value = parseInt(input);
+  if (isNaN(value)) {
+    throw new Error(`Not a valid number`);
+  }
+  return value;
+});
+
+const jsonDebugGroups = makeValidator(input => {
+  const value = JSON.parse(input);
+
+  if (!(value instanceof Array)) {
+    throw new Error(`Not a JSON array`);
+  }
+  for (const elem of value) {
+    if (typeof elem != "string") {
+      throw new Error(`Value must be a string "${elem}"`);
+    }
+  }
+
+  return value;
+});
