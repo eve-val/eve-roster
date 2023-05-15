6f1a943d38bb7824fcb42708c1bfb15003a9baf8
7sempra <net7runner@gmail.com>
2023-05-15 00:02:41 -0400

    Allow members to set their mains with no restriction (#1698)
    We were originally restricting this to a 7-day-from-account-creation
    window as a security measure, but the security concerns never really
    came to fruition.

--- src/domain/account/canDesignateMain.ts
+++ src/domain/account/canDesignateMain.ts
@@ -1,6 +1,12 @@
 import moment from "moment";
 
-const MODIFY_MAIN_WINDOW_DURATION = moment.duration(7, "days").asMilliseconds();
+// Originally we wanted to for main-changes outside of a certain window (7 days)
+// to require admin approval, but we never built the admin approval interface
+// and the security improvement appears to be minimal, so we'll disable this
+// for now.
+const MODIFY_MAIN_WINDOW_DURATION = moment
+  .duration(100, "years")
+  .asMilliseconds();
 
 export function canDesignateMain(accountCreated: number) {
   return Date.now() < accountCreated + MODIFY_MAIN_WINDOW_DURATION;
