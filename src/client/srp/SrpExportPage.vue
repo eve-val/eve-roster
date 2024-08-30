d087d46ba543516880ad32ae20bedb55d55e35ec
Liz Fong-Jones <lizf@honeycomb.io>
2024-08-30 10:26:11 +0200

    prettier

--- src/client/srp/SrpExportPage.vue
+++ src/client/srp/SrpExportPage.vue
@@ -400,7 +400,7 @@ export default defineComponent({
 });
 
 function cleanUserDate(value: string | nil) {
-  return value == "" ? undefined : value ?? undefined;
+  return value == "" ? undefined : (value ?? undefined);
 }
 </script>
 
